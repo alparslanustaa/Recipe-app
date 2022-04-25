00d511f5fc4cbd2325adba54bea65051cef7a6f9
Alparslan Bahadir Usta <93120939+alparslanustaa@users.noreply.github.com>
2022-04-25 05:13:24 -0400

    text and implement of useEffect and UseState for submit button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import './App.css';
 
 const App = () => {
-//API STUFF
+  //API STUFF
   const APP_ID = "66f8e97a";
   const APP_KEY = "a8747c363b12db76e9acd2207edc4ce6";
-  const exampleRequest = 
-  "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}";
+  const exampleRequest =
+    `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  //state stuff
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log('Effect test run');
+  })
 
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <form className="search-form">
+        <input search="form" type="text" />
+        <button
+          onClick={() => setCounter(counter + 1)}
+          className="search-button"
+          type="submit">
+            {counter}
+        </button>
+      </form>
     </div>
   )
 }
