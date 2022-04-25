31de0fa34be95c20ec00f15b861be08a1b376751
Alparslan Bahadir Usta <93120939+alparslanustaa@users.noreply.github.com>
2022-04-25 04:58:32 -0400

    implement api planing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from "react"
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const APP_ID = "66f8e97a";
+  const APP_KEY = "a8747c363b12db76e9acd2207edc4ce6";
+  const exampleRequest = 
+  "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}";
+
   return (
-    <div className></div>
-  );
+    <div className="App">
+      <h1>Hello React</h1>
+    </div>
+  )
 }
 
 export default App;
